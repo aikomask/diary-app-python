381b6dfe9ce4ba9417330bfe971934bb2d67b2b5
aikomask <bekbulatkyzyaigerim@gmail.com>
2025-05-21 04:17:04 +0500

    updated login design

--- diary/urls.py
+++ diary/urls.py
@@ -3,6 +3,7 @@ from . import views
 from diary.views import register
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,6 +12,8 @@ urlpatterns = [
     path('entry/<int:entry_id>/edit/', views.edit_entry, name='edit_entry'),
     path('entry/<int:entry_id>/delete/', views.delete_entry, name='delete_entry'),
     path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='diary/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('entry/<int:entry_id>/export/', views.export_entry_pdf, name='export_entry_pdf'),
     path('toggle-theme/', views.toggle_theme, name='toggle_theme'),
     path('stats/', views.diary_stats, name='diary_stats'),
